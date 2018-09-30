ac3b57e8216a4eabd8cf7985d1fa84a59c929faf
Bastien Ferbu <bastien.ferbu@gmail.com>
2018-09-30 23:20:35 +0200

    Création de la CoupledComponent

---
+++ TP_COO/src/model/CoupledComponent.java
@@ -0,0 +1,24 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+
+public abstract class CoupledComponent {
+	protected ArrayList<AtomicComponent> components;
+	protected ArrayList<String> inputs;
+	protected ArrayList<String> outputs;
+	protected HashMap<String,String> couple_function;
+
+	protected String name;
+	
+	
+	public AtomicComponent(String _name) {
+		name = _name;
+		components = new ArrayList<AtomicComponent>();
+		
+		inputs = new ArrayList<String>();
+		outputs = new ArrayList<String>();
+		couple_function = new HashMap<String,String>();
+	}
+}
